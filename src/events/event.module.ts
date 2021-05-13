383e5c7cd0abbc39d3c26b3c4f38ba8ccb10d158
MSuzu190331 <m.suzuki.0u0@gmail.com>
2021-05-13 23:31:42 +0900

    first commit

--- src/events/event.module.ts
+++ src/events/event.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventController } from './event.contoroller';
 import { EventService } from './event.service';
+import { Event } from './event.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Event])],
   controllers: [EventController],
   providers: [EventService],
 })
--- src/events/event.service.ts
+++ src/events/event.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './event.entity';
 
 @Injectable()
 export class EventService {
+  constructor(
+    @InjectRepository(Event)
+    private readonly repository: Repository<Event>,
+  ) {}
 
   getEvents() {
-    return 'Hello World event!';
+    return this.repository.find();
   }
 
   getEventById() {
